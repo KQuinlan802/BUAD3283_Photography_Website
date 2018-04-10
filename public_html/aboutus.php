42af509577f6324725c9f38f26a826108f12c685
Kaerstin <kaekae02@gmail.com>
2018-04-10 14:56:25 -0500

    started about us page

--- public_html/aboutus.php
+++ public_html/aboutus.php
@@ -1,2 +1,14 @@
 <?php
 echo"About Us";
+?>
+<!DOCTYPE html>
+<html lang=en>
+<head>
+<title><?php echo  $navTitles[array_search($page, $navItems)]; ?></title>
+<link rel="stylesheet" href="style.css">
+<h1> About Me </h1>
+<img src="aboutme.jpeg" alt= "Logo" />
+</head>
+<body>
+  Just a girl with a camera
+</body>

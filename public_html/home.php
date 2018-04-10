799827c82f1e099170b42b252d739b92b0d16f58
Kaerstin <kaekae02@gmail.com>
2018-04-10 12:57:06 -0500

    Added home content and styled

--- public_html/home.php
+++ public_html/home.php
@@ -1,2 +1,25 @@
 <?php
 echo "Home";
+?>
+<!DOCTYPE html>
+<html lang=en>
+<head>
+<title><?php echo  $navTitles[array_search($page, $navItems)]; ?></title>
+<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+<div id="header">
+<h1> Precious Moments Photography</h1>
+</div>
+<div class="article column1">
+  <p> Weddings </p>
+</div>
+<div class="article column2">
+  <p> Portraits </p>
+</div>
+<div class="article column3">
+  <p> Landscapes </p>
+</div>
+</div>
+</body>

b4ec470ea851b60c9e501f9f2d185991a81b409d
Kaerstin <kaekae02@gmail.com>
2018-04-14 18:51:05 -0500

    working on about me

--- public_html/aboutus.php
+++ public_html/aboutus.php
@@ -12,5 +12,8 @@ echo"About Us";
   <p style="text-align:center;">
     Just a girl with a camera
   </p>
+  <p style="text-align:center;">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at tristique magna, in lobortis magna. Pellentesque accumsan dapibus erat at convallis. Pellentesque blandit cursus odio, eu lacinia massa consequat eget. Maecenas aliquet condimentum odio ac mattis. Nulla sodales, dui nec ultrices eleifend, velit nulla elementum ex, dignissim bibendum erat odio nec elit. Nunc pulvinar nulla vel mollis tristique. Mauris ultricies leo sed urna condimentum, quis tincidunt tellus dictum. Suspendisse imperdiet, nisl vel sollicitudin auctor, dolor dui fermentum nisl, non pellentesque enim tellus vitae ante. Nullam auctor suscipit risus, eget tempus eros pretium sed. Aenean sed placerat nisl. Quisque sollicitudin felis mauris, ut condimentum est sollicitudin nec.
+  </p>
   <img src="aboutme.jpeg" alt= "Logo" height="400" width="300"/>
 </body>
